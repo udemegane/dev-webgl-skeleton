72ac4eded31a28c81de66e34d0cc1423ba431803
udemegane <cappuccino1204.ck@gmail.com>
2021-10-20 03:58:11 +0900

    引数でsrc・distを変更できるように

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { type } = require("os");
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    entry: './src/main.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader'
-            },
-            {
-                test: /.glsl$/,
-                use: 'ts-shader-loader'
-            }
+module.exports = (env) => {
+    return {
+        mode: 'development',
+            entry: (typeof env === "undefined")? './' + env.name + '/src/main.ts': './src/main.ts',
+                output: {
+            filename: 'bundle.js',
+                path: path.resolve(__dirname, (typeof env === "undefined")? 'dist-' + env.name : 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader'
+                },
+                {
+                    test: /.glsl$/,
+                    use: 'ts-shader-loader'
+                }
+            ]
+        },
+        resolve: {
+            extensions: [
+                '.ts'
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'MyAppTitle',
+                meta: [
+                    { viewport: 'width=device-width, initial-scale=1' }
+                ],
+            })
         ]
-    },
-    resolve: {
-        extensions: [
-            '.ts'
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '1w183129-6`s Homework',
-            meta: [
-                { viewport: 'width=device-width, initial-scale=1' }
-            ],
-        })
-    ]
+    }
 };
