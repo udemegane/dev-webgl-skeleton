b486a3639f0d3d5accb6511ae254da3d2e4e2feb
udemegane <cappuccino1204.ck@gmail.com>
2021-10-21 02:43:15 +0900

    dev-serverを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,54 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const path = require('path')
+const fs = require("fs");
+const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = () => {
     return {
         mode: 'development',
-        devtool: 'inline-source-map',
-        entry: './src/main.ts',
+        //devtool: 'inline-source-map',
+        entry: path.resolve(appDirectory, 'src/main.ts'),
         output: {
             filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist')
+            path: path.resolve(appDirectory, 'dist'),
+            clean: true,
+            publicPath: '/',
         },
         module: {
             rules: [
                 {
                     test: /\.ts$/,
-                    use: 'ts-loader'
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
                 },
                 {
                     test: /.glsl$/,
-                    use: 'ts-shader-loader'
+                    use: 'ts-shader-loader',
+                    exclude: /node_modules/,
                 }
             ]
         },
         resolve: {
             extensions: [
-                '.ts'
+                '.ts',
+                '.tsx',
+                '.js'
             ]
+        },
+        devServer: {
+            host: "0.0.0.0",
+            port: 8081,
+            static: path.resolve(appDirectory, 'dist'),
+            hot: true,
+            devMiddleware: {
+                publicPath: '/',
+            }
         },
         plugins: [
             new HtmlWebpackPlugin({
@@ -38,10 +60,10 @@ module.exports = () => {
                         content: 'NAME (STUDENT ID)'
                     }
                 ],
-                inlineSource: '.(glsl|ts|js|css)$',
+                //inlineSource: '.(glsl|ts|js|css)$',
                 minify: false,
             }),
-            new HtmlInlineScriptPlugin()
+            //new HtmlInlineScriptPlugin()
         ]
     }
 };
